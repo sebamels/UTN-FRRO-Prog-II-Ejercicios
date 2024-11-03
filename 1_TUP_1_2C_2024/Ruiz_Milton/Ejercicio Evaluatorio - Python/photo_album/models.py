866d35ab6dddbe5aa2629351a08428b4ec0a9ead
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-11-03 20:28:41 -0300

    Actualización: 03/11 20:28

--- 1_TUP_1_2C_2024/Ruiz_Milton/Ejercicio Evaluatorio - Python/photo_album/models.py
+++ 1_TUP_1_2C_2024/Ruiz_Milton/Ejercicio Evaluatorio - Python/photo_album/models.py
@@ -1,10 +1,10 @@
-from . import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class Photo(db.Model):
+    __tablename__ = 'photos'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
     image = db.Column(db.String, nullable=False)
-
-    def repr(self):
-        return f'<Photo {self.title}>'
