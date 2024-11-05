24305230d6601d0d790e6e6a3c48c82f988106f1
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-11-05 17:07:54 -0300

    Actualización: 05/11 17:07

--- 1_TUP_1_2C_2024/Ruiz_Milton/Ejercicio Evaluatorio - Python/photo_album/routes.py
+++ 1_TUP_1_2C_2024/Ruiz_Milton/Ejercicio Evaluatorio - Python/photo_album/routes.py
@@ -14,54 +14,54 @@ def allowed_file(filename):
 
 @main.route('/')
 def index():
-    photos = Photo.query.all()  
+    photos = Photo.query.all()
     return render_template('index.html', photos=photos)
 
 @main.route('/add', methods=['GET', 'POST'])
 def add_photo():
-    form = PhotoForm() 
-    if form.validate_on_submit(): 
+    form = PhotoForm()
+    if form.validate_on_submit():
         title = form.title.data
         description = form.description.data
         image_file = form.image.data
 
-        if image_file and allowed_file(image_file.filename): 
+        if image_file and allowed_file(image_file.filename):
             filename = secure_filename(image_file.filename)
-            image_file.save(os.path.join(UPLOAD_FOLDER, filename))  
+            image_file.save(os.path.join(UPLOAD_FOLDER, filename))
 
             photo = Photo(title=title, description=description, image=filename)
-            db.session.add(photo)  
-            db.session.commit() 
+            db.session.add(photo)
+            db.session.commit()
             flash('Foto agregada con Éxito!', 'success')
-            return redirect(url_for('main.index'))  
+            return redirect(url_for('main.index'))
 
-    return render_template('photo_form.html', form=form)  
+    return render_template('photo_form.html', form=form)
 
 @main.route('/delete/<int:photo_id>', methods=['POST'])
 def delete_photo(photo_id):
-    photo = Photo.query.get_or_404(photo_id)  
-    db.session.delete(photo)  
-    db.session.commit() 
+    photo = Photo.query.get_or_404(photo_id)
+    db.session.delete(photo)
+    db.session.commit()
     flash('Foto borrada con Éxito!', 'success')
-    return redirect(url_for('main.index')) 
+    return redirect(url_for('main.index'))
 
 @main.route('/edit/<int:photo_id>', methods=['GET', 'POST'])
 def edit_photo(photo_id):
-    photo = Photo.query.get_or_404(photo_id)  
-    form = PhotoForm(obj=photo)  
+    photo = Photo.query.get_or_404(photo_id)
+    form = PhotoForm(obj=photo)
 
-    if form.validate_on_submit():  
+    if form.validate_on_submit():
         photo.title = form.title.data
         photo.description = form.description.data
         image_file = form.image.data
 
-        if image_file and allowed_file(image_file.filename): 
+        if image_file and allowed_file(image_file.filename):
             filename = secure_filename(image_file.filename)
             image_file.save(os.path.join(UPLOAD_FOLDER, filename))
-            photo.image = filename 
+            photo.image = filename
 
-        db.session.commit()  
+        db.session.commit()
         flash('Foto editada con Éxito!', 'success')
-        return redirect(url_for('main.index'))  
+        return redirect(url_for('main.index'))
 
-    return render_template('photo_form.html', form=form, photo=photo)  
+    return render_template('photo_form.html', form=form, photo=photo)
